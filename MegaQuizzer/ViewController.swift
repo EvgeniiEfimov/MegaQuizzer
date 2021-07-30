72eac041f481b6a1f3062b7a58de979710893e06
Maxim Goryachkin <maxgoryachkin@mail.ru>
2021-07-30 09:00:08 +0300

    Initial Commit

---
+++ MegaQuizzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MegaQuizzer
+//
+//  Created by Максим on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
