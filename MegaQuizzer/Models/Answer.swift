de5b9abe36f53f1b68b6ed746295729f5d17c804
kristyashh <62707518+kristyashh@users.noreply.github.com>
2021-07-30 22:20:51 +0300

    models created

---
+++ MegaQuizzer/Models/Answer.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  MegaQuizzer
+//
+//  Created by Kristina Shlyapkina on 30.07.2021.
+//
+
+import Foundation
+
+struct Answer {
+    var answerText: String //Текст ответа
+    var isTrue: Bool //Здесь правильность ответа отслеживается
+}
