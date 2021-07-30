d169a06c1377f084a707509466cede1b36d853dc
kristyashh <62707518+kristyashh@users.noreply.github.com>
2021-07-30 23:06:52 +0300

    Feature/model (#1)

---
+++ MegaQuizzer/Models/QuestionCard.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionCard.swift
+//  MegaQuizzer
+//
+//  Created by Kristina Shlyapkina on 30.07.2021.
+//
+
+struct QuestionCard {
+    var number: Int
+    var questionText: String
+    var answers: [Answer]  
+}
---
+++ MegaQuizzer/Models/Quiz.swift
@@ -0,0 +1,13 @@
+//
+//  Quiz.swift
+//  MegaQuizzer
+//
+//  Created by Kristina Shlyapkina on 30.07.2021.
+//
+
+import Foundation
+
+struct Quiz {
+   var name: String //Название викторины
+   var questions: [QuestionCard] = [] //Объект из массива вопросов викторины
+}
