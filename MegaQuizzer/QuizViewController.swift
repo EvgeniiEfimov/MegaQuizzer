b24b3d1768bad3dad706db3a221e09796626f3e0
EvgeniiEfimov <82396606+EvgeniiEfimov@users.noreply.github.com>
2021-08-01 12:42:38 +0300

    create logic choices

--- MegaQuizzer/QuizViewController.swift
+++ MegaQuizzer/QuizViewController.swift
@@ -11,22 +11,23 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var numberQuestionLabel: UILabel!
     
     @IBOutlet weak var textQuestionsLabel: UILabel!
+        
+    @IBOutlet var answerButtonArray: [UIButton]!
     
-    @IBOutlet weak var answerOneOutlet: UIButton!
-    @IBOutlet weak var answerTwoOutlet: UIButton!
-    @IBOutlet weak var answerThreeOutlet: UIButton!
     
     var quiz = QuizDataManager.shared.getQuizzes()[0] //времянка для теста
-    
-    var indexQuestions = 0
     var score = 0
-
+    var name: String!
     
+    private var indexQuestions = 0
+    private var arrayAnswerPerson: [UIButton] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         settingUI()
         data()
+        numberAnswerText()
     }
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //        guard <#condition#> else {
@@ -34,58 +35,75 @@ class QuizViewController: UIViewController {
 //        }
 //    }
     
-    func settingUI() {
-        answerOneOutlet.layer.cornerRadius = 10
-        answerTwoOutlet.layer.cornerRadius = 10
-        answerThreeOutlet.layer.cornerRadius = 10
-        
-        
-    }
-    
-    func data() {
-        
-        numberQuestionLabel.text = "Вопрос № - \(indexQuestions + 1)"
-        textQuestionsLabel.text = quiz.questions[indexQuestions].questionText
-        
-        numberAnswerText(button: answerOneOutlet, indexAnswer: 0)
-        numberAnswerText(button: answerTwoOutlet, indexAnswer: 1)
-        numberAnswerText(button: answerThreeOutlet, indexAnswer: 2)
-    }
-    
-    func numberAnswerText (button: UIButton, indexAnswer: Int) {
-        button.setTitle(quiz.questions[indexQuestions].answers[indexAnswer].answerText, for: .normal)
-    }
-    
-    
-    func nextQuestions() {
-        if indexQuestions < quiz.questions.count {
-            indexQuestions += 1
-        }
-    }
-    
     @IBAction func answerButtonAction(_ sender: UIButton) {
-        if sender.backgroundColor == UIColor.white {
-            sender.backgroundColor = UIColor.yellow
-        } else if sender.backgroundColor == UIColor.yellow {
-        sender.backgroundColor = UIColor.white
-        }
-    }
-    
-    func check(button: UIButton, indexAnswer: Int) {
-         guard button.backgroundColor == UIColor.yellow else { return }
-         quiz.questions[indexQuestions].answers[indexAnswer].isTrue ?
-            (score += 100) : (score -= 50)
-        button.backgroundColor = UIColor.white
+            sender.backgroundColor == UIColor.white ?
+            (sender.backgroundColor = UIColor.yellow) :
+            (sender.backgroundColor = UIColor.white)
     }
     
     @IBAction func nextButtonAction() {
-        check(button: answerOneOutlet, indexAnswer: 0)
-        check(button: answerTwoOutlet, indexAnswer: 1)
-        check(button: answerThreeOutlet, indexAnswer: 2)
-        
-        nextQuestions()
-        data()
-        
+        if quiz.questions.count > indexQuestions {
+            check()
+            if arrayAnswerPerson.count == 0 {
+                alertPresent()
+            } else {
+                nextQuestions()
+                data()
+            }
         print(score)
+        } else {
+//            performSegue(withIdentifier: <#T##String#>, sender: nil) // <- переход на результат
+        }
    }
+   private func settingUI() {
+        for answerButton in answerButtonArray {
+            answerButton.layer.cornerRadius = 10
+            answerButton.isHidden = true
+        }
+        
+        
+    }
+    
+   private func data() {
+        numberQuestionLabel.text = "Вопрос № - \(indexQuestions + 1)"
+        textQuestionsLabel.text = quiz.questions[indexQuestions].questionText
+        numberAnswerText()
+            }
+    
+   private func numberAnswerText () {
+        for (index,value) in quiz.questions[indexQuestions].answers.enumerated() {
+            answerButtonArray[index].setTitle(value.answerText, for: .normal)
+            answerButtonArray[index].isHidden = false
+        }
+    }
+    
+    private func nextQuestions() {
+        if indexQuestions < quiz.questions.count {
+            indexQuestions += 1
+            arrayAnswerPerson.removeAll()
+        }
+    }
+    private func alertPresent() {
+        let alert = UIAlertController(title: nil,
+                                      message:  "А ответ кто будет выбиарть?",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Я",
+                                      style: .cancel,
+                                      handler: nil))
+        self.present(alert,
+                     animated: true,
+                     completion: nil)
+        }
+    
+    private func check() {
+        for (index, value) in answerButtonArray.enumerated() {
+            if value.backgroundColor == UIColor.yellow {
+            arrayAnswerPerson.append(value)
+            quiz.questions[indexQuestions].answers[index].isTrue ?
+                (score += 100) : (score -= 50)
+                value.backgroundColor = UIColor.white
+            }
+        }
+    }
 }
